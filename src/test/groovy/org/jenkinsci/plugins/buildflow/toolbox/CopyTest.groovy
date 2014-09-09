e18a561bab4cefdeffe0c86c88703f3acea63238
Damien Nozay <damien.nozay@gmail.com>
2014-09-09 02:57:13 -0700

    fix tests

--- src/test/groovy/org/jenkinsci/plugins/buildflow/toolbox/CopyTest.groovy
+++ src/test/groovy/org/jenkinsci/plugins/buildflow/toolbox/CopyTest.groovy
@@ -1,10 +1,15 @@
 package org.jenkinsci.plugins.buildflow.toolbox
 
+import org.jenkinsci.plugins.buildflow.toolbox.BuildFlowToolbox
+import com.cloudbees.plugins.flow.BuildFlow
+import com.cloudbees.plugins.flow.FlowRun
+import com.cloudbees.plugins.flow.BuildFlowDSLExtension
 import com.cloudbees.plugins.flow.DSLTestCase
 import static hudson.model.Result.SUCCESS
 import hudson.model.Job
 import hudson.tasks.Shell
 import hudson.FilePath
+import jenkins.model.Jenkins
 
 
 /**
@@ -12,19 +17,29 @@ import hudson.FilePath
  */
 class CopyTest extends DSLTestCase {
     public void testUseExtension() {
+        // No signature of method: java.util.TreeMap.copyFiles
+        // happens when TestDSLExtension is already present.
+        BuildFlowDSLExtension.all().clear()
+        // we need to insert ourselves here so symbols are found.
+        BuildFlowDSLExtension.all().add(new BuildFlowToolbox())
+
         Job job1 = createJob("job1")
         job1.getBuildersList().add(new Shell("echo hello > artifact"));
         // https://github.com/jenkinsci/jenkins/blob/HEAD/test/src/test/groovy/hudson/model/AbstractProjectTest.groovy#L257
 
-        def flow = run("""
+        BuildFlow flow = new BuildFlow(Jenkins.instance, getName())
+        flow.buildNeedsWorkspace = true
+        flow.onCreatedFromScratch()
+        flow.dsl = """
             def toolbox = extension.'build-flow-toolbox'
             b = build("job1")
             toolbox.copyFiles(b.workspace, build.workspace)
-        """)
-        System.out.println flow.log
-        assert SUCCESS == flow.result
-        assert flow.log.contains("Copying files from")
-        FilePath[] list = flow.getWorkspace().list()
-        assert list == ['artifact']
+        """
+        FlowRun flowRun = flow.scheduleBuild2(0).get()
+        assert SUCCESS == flowRun.result
+        assert flowRun.log.contains("Copying files from")
+        FilePath[] list = flowRun.getWorkspace().list()
+        assert list.size() == 1
+        assert list[0].name == 'artifact'
     }
 }
